112432e91644153683fa337285de47e4bea6ca4e
drvarun <12458730+drvarun@users.noreply.github.com>
2021-06-07 14:08:55 +0200

    minor fixes

--- keygen/gen_certi.py
+++ keygen/gen_certi.py
@@ -2,7 +2,7 @@ from Crypto.PublicKey import ECC
 from Crypto.Hash import SHA256
 from Crypto.Signature import DSS
 
-f = open('uniris-ca.pem','rt')
+f = open('uniris-ca.pem', 'rt')
 signing_key = ECC.import_key(f.read())
 
 keys = open('keys.txt', 'r').readlines()
@@ -11,7 +11,7 @@ for key in keys:
     binary_key = bytes.fromhex(key)
     key_hash = SHA256.new(binary_key)
 
-    uniris_ca = DSS.new(signing_key,'fips-186-3','der')
+    uniris_ca = DSS.new(signing_key, 'fips-186-3', 'der')
     mini_certificate = uniris_ca.sign(key_hash)
 
     f = open('certificates/'+key_hash.hexdigest()+'.bin', 'wb')
