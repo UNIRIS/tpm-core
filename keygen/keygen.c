1a677d0a7611319e0b6891e8d2e1d47dc4c43ba0
drvarun <12458730+drvarun@users.noreply.github.com>
2021-06-07 13:11:58 +0200

    minor fixes

---
+++ keygen/keygen.c
@@ -0,0 +1,28 @@
+/*  Compile:
+    gcc keygen.c -o keygen uniris-tpm.c -ltss2-esys -lcrypto
+
+    Keygen:
+    sudo ./keygen > keys.txt
+*/
+
+#include <stdio.h>
+#include <openssl/sha.h>
+#include "../uniris-tpm.h"
+
+void main()
+{
+    initializeTPM(1);
+
+    INT publicKeySize = 0;
+    BYTE *asnkey;
+
+    for (int z = 0; z < 50; z++)
+    {
+        asnkey = getPublicKey(z, &publicKeySize);
+        for (int v = 26; v < publicKeySize; v++)
+        {
+            printf("%02x", asnkey[v]);
+        }
+        printf("\n");
+    }
+}
