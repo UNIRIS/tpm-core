8e38489e5631a17c59b6cec2e2de22005ac0c1a1
drvarun <12458730+drvarun@users.noreply.github.com>
2021-06-18 18:22:29 +0200

    installation script

---
+++ tpm_install/tpm_install.sh
@@ -0,0 +1,45 @@
+#/bin/bash
+
+#assumes Elixir and Erlang are alaredy installed on the system
+mkdir tpm
+cd tpm
+
+sudo apt -y update
+sudo apt -y install \
+  autoconf-archive \
+  libcmocka0 \
+  libcmocka-dev \
+  procps \
+  iproute2 \
+  build-essential \
+  git \
+  pkg-config \
+  gcc \
+  libtool \
+  automake \
+  libssl-dev \
+  uthash-dev \
+  autoconf \
+  doxygen \
+  libjson-c-dev \
+  libini-config-dev \
+  libcurl4-openssl-dev \
+  git
+
+git clone https://github.com/tpm2-software/tpm2-tss.git
+cd tpm2-tss
+./bootstrap
+./configure --with-udevrulesdir=/etc/udev/rules.d
+sudo sed -i 's/tss/uniris/gi' /etc/udev/rules.d/tpm-udev.rules
+make -j$(nproc)
+
+sudo make install
+sudo udevadm control --reload-rules && sudo udevadm trigger
+sudo ldconfig
+sudo apt install tpm2-tools
+
+cd ..
+git clone https://github.com/UNIRIS/tpm-core.git
+cd tpm-core
+gcc support.c -o support stdio_helpers.c uniris-tpm.c -ltss2-esys
+cd ../..
