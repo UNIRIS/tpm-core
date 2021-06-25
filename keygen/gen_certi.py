b985d953be28709450c835dddebc5c7375067fec
drvarun <12458730+drvarun@users.noreply.github.com>
2021-06-25 14:25:34 +0200

    minor fixes

--- keygen/gen_certi.py
+++ keygen/gen_certi.py
@@ -1,3 +1,5 @@
+# pycryptodome required: sudo pip install pycryptodome
+import os, shutil
 from Crypto.PublicKey import ECC
 from Crypto.Hash import SHA256
 from Crypto.Signature import DSS
@@ -5,15 +7,22 @@ from Crypto.Signature import DSS
 f = open('uniris-ca.pem', 'rt')
 signing_key = ECC.import_key(f.read())
 
-keys = open('keys.txt', 'r').readlines()
+for root, dirs, files in os.walk('keys', topdown=False):
+    for name in files:
+        key_file = os.path.join(name)
+        os.mkdir('certificates/'+key_file)
+        keys = open('keys/'+key_file, 'r').readlines()
 
-for key in keys:
-    binary_key = bytes.fromhex(key)
-    key_hash = SHA256.new(binary_key)
+        for key in keys:
+            binary_key = bytes.fromhex(key)
+            key_hash = SHA256.new(binary_key)
 
-    uniris_ca = DSS.new(signing_key, 'fips-186-3', 'der')
-    mini_certificate = uniris_ca.sign(key_hash)
+            uniris_ca = DSS.new(signing_key, 'fips-186-3', 'der')
+            mini_certificate = uniris_ca.sign(key_hash)
 
-    f = open('certificates/'+key_hash.hexdigest()+'.bin', 'wb')
-    f.write(mini_certificate)
-    f.close()
+            f = open('certificates/'+key_file+'/'+key_hash.hexdigest()+'.bin', 'wb')
+            f.write(mini_certificate)
+            f.close()
+        
+        shutil.make_archive('certificates/'+key_file, 'zip', 'certificates/'+key_file)
+        shutil.rmtree('certificates/'+key_file)
