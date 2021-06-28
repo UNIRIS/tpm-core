4b5a71d1e846d2f870bd01bbb35c21ec4af54366
drvarun <12458730+drvarun@users.noreply.github.com>
2021-06-28 18:56:14 +0200

    added support for ECDH

--- driver.c
+++ driver.c
@@ -20,6 +20,8 @@ void main()
     //time_taken = ((double)t) / CLOCKS_PER_SEC; // in seconds
     //printf("\ngetPublicKey() took %f seconds to execute \n", time_taken);
 
+    BYTE eph_pub[65] = {0x04, 0x1F, 0x51, 0xAE, 0x5D, 0x01, 0xB8, 0xAC, 0x0D, 0x3F, 0x12, 0xF1, 0xF6, 0x35, 0xCF, 0x70, 0xD9, 0x77, 0x2B, 0x47, 0x69, 0x15, 0xBC, 0xF5, 0x77, 0xE9, 0x3D, 0x82, 0xEE, 0x12, 0x53, 0x05, 0x62, 0x7C, 0x71, 0x0A, 0x7B, 0x78, 0x5B, 0x3D, 0x15, 0x56, 0x83, 0xB4, 0xF9, 0x92, 0x6F, 0x3E, 0xC2, 0x49, 0xA6, 0x1F, 0x53, 0x4B, 0x8E, 0x2F, 0xA5, 0x31, 0xDA, 0xC9, 0x70, 0x3A, 0x81, 0xB6, 0x82};
+    BYTE *zPoint;
     for (int i = 1; i <= 4; i++)
     {
 
@@ -29,6 +31,8 @@ void main()
         {
             printf("%02x", asnkey[v]);
         }
+        
+        
         printf("\n\nSign Key Index: %d", getKeyIndex());
         eccSign = signECDSA(i, hash256, &signLen, true);
         printf("\nECC Sign (ASN.1 DER) = \n");
@@ -36,6 +40,14 @@ void main()
         {
             printf("%02x", eccSign[v]);
         }
+
+        zPoint = getECDHPoint(i, eph_pub);
+        printf("\n\nZ Point (Uncompressed/Raw) = \n");
+        for (int v = 0; v < 65; v++)
+        {
+            printf("%02x", zPoint[v]);
+        }
+
         printf("\nAfter Sign Key Index: %d", getKeyIndex());
 
         asnkey = getPublicKey(i + 1, &publicKeySize);
@@ -44,7 +56,7 @@ void main()
         {
             printf("%02x", asnkey[v]);
         }
-        printf("\n----------------------------------------------------------------------");
+        printf("\n\n----------------------------------------------------------------------");
     }
-    printf("\n");
+    printf("\n\n");
 }
